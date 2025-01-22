9c25cc5515166d71afb9ef356a41a240c173fc9e
JustVibin247 <72247863+JustVibin247@users.noreply.github.com>
2025-01-22 10:32:27 -0600

    pls work pls

--- common/src/main/java/net/mehvahdjukaar/amendments/client/renderers/CandleHolderRendererExtension.java
+++ common/src/main/java/net/mehvahdjukaar/amendments/client/renderers/CandleHolderRendererExtension.java
@@ -98,10 +98,52 @@ public class CandleHolderRendererExtension implements IThirdPersonAnimationProvi
 
     private static final ResourceLocation FLAME = new ResourceLocation("textures/particle/flame.png");
     private static final ResourceLocation FLAME_SOUL = new ResourceLocation("textures/particle/soul_fire_flame.png");
+    private static final ResourceLocation FLAME_ENDER = new ResourceLocation("textures/particle/ender_fire_flame.png");
+    private static final ResourceLocation FLAME_CUPRIC = new ResourceLocation("textures/particle/cupric_fire_flame.png");
 
     private static void renderFlame(LivingEntity entity, PoseStack poseStack, MultiBufferSource bufferSource, ItemStack stack) {
         boolean soul = stack.getItem() == CompatObjects.SOUL_CANDLE_HOLDER.get();
         var builder = bufferSource.getBuffer(RenderType.text(soul ? FLAME_SOUL : FLAME));
+
+        int lu = LightTexture.FULL_BRIGHT & '\uffff';
+        int lv = LightTexture.FULL_BRIGHT >> 16 & '\uffff';
+
+        float period = 20;
+        float t = ((entity.tickCount + Minecraft.getInstance().getFrameTime()) % period) / period;
+        float ss = (1.0F - t * t * 0.4F);
+
+        float scale = ss * 2 / 16f;
+
+        poseStack.translate(0, 3 / 16f, 0);
+        poseStack.last().pose().setRotationXYZ(0,0,0);
+        poseStack.scale(-scale, scale, -scale);
+
+        VertexUtil.addQuad(builder, poseStack, -0.5f, -0.5f, 0.5f, 0.5f, lu, lv);
+    }
+
+    private static void renderFlame(LivingEntity entity, PoseStack poseStack, MultiBufferSource bufferSource, ItemStack stack) {
+        boolean soul = stack.getItem() == CompatObjects.ENDER_CANDLE_HOLDER.get();
+        var builder = bufferSource.getBuffer(RenderType.text(ender ? FLAME_ENDER : FLAME));
+
+        int lu = LightTexture.FULL_BRIGHT & '\uffff';
+        int lv = LightTexture.FULL_BRIGHT >> 16 & '\uffff';
+
+        float period = 20;
+        float t = ((entity.tickCount + Minecraft.getInstance().getFrameTime()) % period) / period;
+        float ss = (1.0F - t * t * 0.4F);
+
+        float scale = ss * 2 / 16f;
+
+        poseStack.translate(0, 3 / 16f, 0);
+        poseStack.last().pose().setRotationXYZ(0,0,0);
+        poseStack.scale(-scale, scale, -scale);
+
+        VertexUtil.addQuad(builder, poseStack, -0.5f, -0.5f, 0.5f, 0.5f, lu, lv);
+    }
+
+    private static void renderFlame(LivingEntity entity, PoseStack poseStack, MultiBufferSource bufferSource, ItemStack stack) {
+        boolean soul = stack.getItem() == CompatObjects.CUPRIC_CANDLE_HOLDER.get();
+        var builder = bufferSource.getBuffer(RenderType.text(cupric ? FLAME_CUPRIC : FLAME));
 
         int lu = LightTexture.FULL_BRIGHT & '\uffff';
         int lv = LightTexture.FULL_BRIGHT >> 16 & '\uffff';
